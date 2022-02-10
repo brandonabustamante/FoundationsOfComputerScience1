fe068cc85d36a8594415856b44728fc6fd2bf319
Brandon A Bustamante <brandonabus1995@gmail.com>
2022-02-09 18:02:35 -0600

    Lab 1.21 complete

--- README.md
+++ README.md
@@ -8,5 +8,10 @@ to Java and Python and add them to the repository.
 
 ## Lab 1.20 Basic output with variables
 The programming assignment is in C++. The goal is to get practice with basic
- input and output. I make use of the power function for better readability.
- I will convert this code to Java and Python as well.
+input and output. I make use of the power function for better readability.
+I will convert this code to Java and Python as well.
+
+## Lab 1.21 Quadratic equation discriminant
+The programming assignment is in C++. The goal is to compute the quadratic
+equation. I made use of the power function once again for ease of reading.
+I will convert to Java and Python for additional practice.
---
+++ Unit1Labs/lab1.21.cpp
@@ -0,0 +1,46 @@
+// Brandon Aaron Bustamante, yxx5@txstate.edu
+// No collaborator(s)
+// LAb 1.20.1
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*
+    * To run with autotest cases, leave the code as-is.
+    * For manual input, uncomment "cin >> a, cin >> b,
+    *  cin >> c" then run the code.
+*/
+void quadraticEquation(int a, int b, int c){
+    //cin >> a;
+    //cin >> b;
+    //cin >> c;
+    // The quadratic equation is (b*b) - (4*a*c)
+    // I make use of the power function 
+    int quadEq = (pow(b,2) - (4 * (a * c)));
+    cout << quadEq << endl;
+}
+
+int main() {
+
+    int a, b, c;
+
+    // Test 0 
+    a = 2;
+    b = 5;
+    c = 6;
+    quadraticEquation(a,b,c);
+
+    // Test 1
+    a = 8;
+    b = 100;
+    c = 25;
+    quadraticEquation(a,b,c);
+
+    // Test 2
+    a = 11;
+    b = -42;
+    c = 5;
+    quadraticEquation(a,b,c);
+
+   return 0;
+}
