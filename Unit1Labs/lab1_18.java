1dba148c8b6cff6efa789f4edb0fa56932ed4613
Brandon A Bustamante <brandonabus1995@gmail.com>
2022-04-24 20:12:38 -0500

    Lab1_18 completed in C++, Java, and Python.

---
+++ Unit1Labs/lab1_18.java
@@ -0,0 +1,35 @@
+// Comment out the package to run Java files in your system.
+package Unit1Labs;
+// Brandon Aaron Bustamante, yxx5@txstate.edu
+// No collaborators
+// Lab 1.18 Formatted output: ASCII art (bicyclist)
+
+/*
+ * Program Specifications Pictures made from keyboard characters
+ * are known as ASCII art and can be quite intricate. ASCII Art 
+ * Archive provides examples. Write a program that outputs a drawing
+ * of a bicyclist using cout. For ALL labs, end the last line with
+ * newline (unless otherwise stated).
+
+ * Note: a backslash \ in a string acts as an escape character, such
+ * as with a newline \n. So, to print a single backslash, include two
+ * backslashes next to each other. Ex: The following prints a single
+ * backslash: cout << "\\";
+ * The output is (note, this is three lines of text):
+ *  __o
+ *  _ \<_
+ * (_)/(_)
+*/
+// Do not exceed last *
+/********************************************************************************/
+
+class lab1_18 {
+    public static void main(String[] args) {
+        
+        System.out.println("   __o" );
+        System.out.println(" _ \\<_");
+        System.out.println("(_)/(_)");
+
+        System.exit(0);
+    }
+}
---
+++ Unit1Labs/lab1_18.py
@@ -0,0 +1,33 @@
+# Brandon Aaron Bustamante, yxx5@txstate.edu
+# No collaborators
+# Lab 1.18 Formatted output: ASCII art (bicyclist)
+
+""""
+ * Program Specifications Pictures made from keyboard characters
+ * are known as ASCII art and can be quite intricate. ASCII Art 
+ * Archive provides examples. Write a program that outputs a drawing
+ * of a bicyclist using cout. For ALL labs, end the last line with
+ * newline (unless otherwise stated).
+
+ * Note: a backslash \ in a string acts as an escape character, such
+ * as with a newline \n. So, to print a single backslash, include two
+ * backslashes next to each other. Ex: The following prints a single
+ * backslash: cout << "\\";
+ * The output is (note, this is three lines of text):
+ *  __o
+ *  _ \<_
+ * (_)/(_)
+"""
+# Do not exceed last *
+#********************************************************************************#
+
+if __name__ == "__main__":
+   
+    print("   __o" )
+    print(" _ \\<_")
+    print("(_)/(_)")
+
+    quit()
+
+
+
