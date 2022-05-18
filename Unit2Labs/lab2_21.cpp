211db8c99e99858e320609fd9b4e11fd92b2603b
Brandon A Bustamante <brandonabus1995@gmail.com>
2022-05-17 20:27:41 -0500

    Starting lab 2.21

---
+++ Unit2Labs/lab2_21.cpp
@@ -0,0 +1,19 @@
+// Brandon A Bustamante yxx5@txstate.edu
+// No collaborators
+// LAB 2.21
+
+/*
+ * People find it easier to read time in hours, minutes, and seconds rather than
+ * just seconds.
+ * Write a program that reads in seconds as input, and outputs the time in hours,
+ * minutes, and seconds.
+ * Ex: If the input is:
+ * 4000
+ * the output is:
+ * Hours: 1
+ * Minutes: 6
+ * Seconds: 40
+*/
+
+// Do not exceed last * 
+/********************************************************************************/
---
+++ Unit2Labs/lab2_21.py
@@ -0,0 +1,10 @@
+# Brandon A Bustamante yxx5@txstate.edu
+# No collaborators
+# LAB 2.21
+
+""""
+ *
+"""
+
+# Do not exceed last * 
+#********************************************************************************#
